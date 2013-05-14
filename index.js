232fb0185a294fe7a6dfa2d27426113c83718804
Lance Pollard <lancejpollard@gmail.com>
2013-05-14 00:41:13 -0700

    basics

--- index.js
+++ index.js
@@ -3,13 +3,30 @@
  * Module dependencies.
  */
 
-var operator = require('tower-operator');
+var operator = require('tower-operator')
+  , escapeRegExp = 'undefined' === typeof window
+    ? require('escape-regexp-component')
+    : require('escape-regexp');
 
 /**
  * Expose `expression`.
  */
 
 module.exports = expression;
+
+/**
+ * RegExps.
+ */
+
+var filterRegExp = / +| +/;
+var fnRegExp = /(\w+)\(([^\)]*)\)/;
+var numberRegExp = /^\d+(?:\.\d+)*$/
+var propertyRegExp = /[\w\d\.]+/
+var operatorRegExp = [];
+for (var i = 0, n = operator.collection.length; i < n; i++) {
+  operatorRegExp.push(escapeRegExp(operator.collection[i]));
+}
+operatorRegExp = new RegExp('(' + propertyRegExp.source + ') *(' + operatorRegExp.join('|') + ') *(' + propertyRegExp.source + ')');
 
 /**
  * Parse a directive expression.
@@ -22,28 +39,79 @@ module.exports = expression;
  */
 
 function expression(val) {
-  if (val.indexOf(':') != -1) {
-    return keyValueExpression(val);
-  } else if (val.indexOf('(') != -1) {
-    return functionExpression(val);
-  }
+  return Function('scope', '  return ' + parseExpression(val));
+}
 
-  return Function('scope', 'return 1 + 1');
+function filterExpression(val) {
+  val = val.split(filterRexExp);
+  for (var i = 0, n = val.length; i < n; i++) {
+    // XXX
+    // val[i] = x
+  }
+  return val
+}
+
+function parseExpression(val) {
+  return keyValueExpression(val)
+    || fnExpression(val)
+    || operatorExpression(val)
+    || propertyExpression(val);
 }
 
 // <input on-keypress="enter:createTodo">
 // <input on-keypress="enter : createTodo">
+// <input on-keypress="enter:create(todo)">
 function keyValueExpression(val) {
-  val = val.split(/ +: +/);
-  return { key: val[0], val: expression(val[0]) };
+  // XXX: todo
+  // val.match(fnRegExp);
 }
 
-// <input on-keypress="enter:create(todo)">
-// <input on-keypress="[enter] create(todo)">
-function functionExpression(val) {
+// <input on-keypress="create(todo)">
+function fnExpression(val) {
+  if (!val.match(fnRegExp)) return;
 
+  var name = RegExp.$1;
+  var args = RegExp.$2;
+  
+  if (args) {
+    return "scope.call('" + name + "', " + argumentsExpression(args) + ")";
+  } else {
+    return "scope.call('" + name + "')";
+  }
 }
 
 function argumentsExpression(val) {
+  val = val.split(/ *, */);
+  var result = [];
+  for (var i = 0, n = val.length; i < n; i ++) {
+    result.push(parseExpression(val[i]));
+  }
+  return result.join(', ');
+}
 
+function operatorExpression(val) {
+  if (!val.match(operatorRegExp)) return;
+
+  var left = RegExp.$1;
+  var operator = RegExp.$2;
+  var right = RegExp.$3;
+
+  var code = parseExpression(left)
+    + ' ' + operator + ' '
+    + parseExpression(right);
+
+  return code;
+}
+
+function propertyExpression(val) {
+  return numberExpression(val)
+    || pathExpression(val);
+}
+
+function numberExpression(val) {
+  if (val.match(numberRegExp)) return val;
+}
+
+function pathExpression(val) {
+  return "scope.get('" + val + "')";
 }
--- test/index.js
+++ test/index.js
@@ -6,10 +6,80 @@ var assert = 'undefined' == typeof window
   ? require('assert')
   : require('component-assert');
 
+var scopes = require('tower-scope');
+
 describe('expression', function(){
-  it('should test', function(){
-    var fn = expression('1 + 1');
-    console.log(fn.toString())
-    assert(2 === fn());
+  describe('operators', function(){
+    it('should parse simple math expression', function(){
+      var fn = expression('1 + 1');
+      assert(2 === fn());
+      var fn = expression('2 * 2');
+      assert(4 === fn());
+    });
+
+    it('should parse >', function(){
+      var fn = expression('1 > 2');
+      assert(false === fn());
+    });
+
+    it('should parse >=', function(){
+      var fn = expression('1 >= 2');
+      assert(false === fn());
+      var fn = expression('1 >= 1');
+      assert(true === fn());
+    });
+
+    it('should parse <', function(){
+      var fn = expression('1 < 2');
+      assert(true === fn());
+    });
+
+    it('should parse <=', function(){
+      var fn = expression('2 <= 2');
+      assert(true === fn());
+      var fn = expression('2 <= 1');
+      assert(false === fn());
+    });
+  });
+
+  it('should parse attribute', function(){
+    var fn = expression('createTodo');
+    var string = [
+        "function anonymous(scope) {"
+      , "  return scope.get('createTodo')"
+      , "}"
+    ].join('\n');
+    assert(string === fn.toString());
+  });
+
+  describe('function', function(){
+    it('should parse method', function(){
+      var fn = expression('createTodo()');
+      var string = [
+          "function anonymous(scope) {"
+        , "  return scope.call('createTodo')"
+        , "}"
+      ].join('\n');
+      assert(string === fn.toString());
+    });
+
+    it('should parse method with arguments', function(done){
+      var fn = expression('create(todo)');
+      var string = [
+          "function anonymous(scope) {"
+        , "  return scope.call('create', scope.get('todo'))"
+        , "}"
+      ].join('\n');
+      assert(string === fn.toString());
+
+      scopes('todo')
+        .action('create', function(todo){
+          assert('A todo!' === todo.title);
+          done();
+        });
+
+      var ctx = scopes('todo').init({ todo: { title: 'A todo!' }});
+      fn(ctx);
+    });
   });
 });
