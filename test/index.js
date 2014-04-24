e06b4a81a1b3f93000b8757c163d0a8474439f7e
Lance Pollard <lancejpollard@gmail.com>
2014-04-23 21:21:07 -0700

    pass tests for nested expressions with stack algorithm

--- test/index.js
+++ test/index.js
@@ -8,24 +8,15 @@ if ('undefined' === typeof window) {
 }
 
 describe('expression', function(){
-  // it('should define expression', function(){
-  //   var exp = new Expression('addition');
-  //   exp.match(/\d+/, ' + ', /\d+/, addition);
-  //   var val = exp.parse('1 + 2');
-  //   assert('addition' == exp.name);
-  //   assert(3 === val);
-  // });
+  it('should define string expressions', function(){
+    var grammar = new Expression('math');
+    var expression = grammar.expression;
 
-  // it('should use sub-expressions', function(){
-  //   var a = new Expression('plus').match('+');
-  //   var b = new Expression('numb').match(/\d/);
-  //   var c = new Expression('math').match(':numb', ':plus', ':numb', addition);
-  //   c.use(a);
-  //   c.use(b);
+    expression('math').match('1', parseInt);
 
-  //   var val = c.parse('1+2');
-  //   assert(3 === val);
-  // });
+    var val = grammar.parse('1');
+    assert(1 === val);
+  });
 
   it('should define sub-expressions', function(){
     var grammar = new Expression('math');
@@ -36,18 +27,7 @@ describe('expression', function(){
     expression('numb').match(/\d/, value);
 
     var val = grammar.parse('1+2');
-    console.log(val);
-    //assert(3 === val);
-  });
-
-  it('should define string expressions', function(){
-    var grammar = new Expression('math');
-    var expression = grammar.expression;
-
-    expression('math').match('1', parseInt);
-
-    var val = grammar.parse('1');
-    assert(1 === val);
+    assert(3 === val);
   });
 
   // it('should define nested expressions', function(){
@@ -60,8 +40,6 @@ describe('expression', function(){
   //   var val = grammar.parse('123');
   //   console.log(val);
   // });
-
-  // it('should define grammar');
 });
 
 function addition($1, $2, $3) {
